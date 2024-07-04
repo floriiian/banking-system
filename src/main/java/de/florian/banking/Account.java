17c0651a67f7a0bf4e3d79edcf752b401d7b663b
floriiian <florianswzz@gmail.com>
2024-07-04 19:26:30 +0200

    First

--- src/main/java/de/florian/banking/Account.java
+++ src/main/java/de/florian/banking/Account.java
@@ -1,4 +1,29 @@
 package de.florian.banking;
 
 public class Account {
+
+    public String name;
+    public int age;
+    public long balance;
+    public int accountId;
+
+    // Constructor
+    public Account(String name, int age, long balance, int accountId) {
+        this.name = name;
+        this.age = age;
+        this.balance = balance;
+        this.accountId = accountId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
 }
--- src/main/java/de/florian/banking/Main.java
+++ src/main/java/de/florian/banking/Main.java
@@ -1,19 +1,52 @@
 package de.florian.banking;
 
+import io.javalin.http.staticfiles.Location;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import io.javalin.Javalin;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
 
 
 public class Main {
+
+    public static ArrayList<Account> accounts = new ArrayList<>();
+    public static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
 
-        final Logger LOGGER = LogManager.getLogger();
-
-        var app = Javalin.create(/*config*/)
-                .get("/", ctx -> ctx.result("Hello World"))
+        Javalin app = Javalin.create(config -> {
+                    config.staticFiles.add("/public", Location.CLASSPATH);
+                })
                 .start(7070);
 
-        LOGGER.debug("Banking System started!");
+        app.get("/", ctx -> ctx.redirect("/index.html"));
+
+
+        addAccount("Brian", 99);
+        addAccount("Florian", 20);
+
     }
+
+    public static void addAccount(String name, int age) {
+        Account newAccount = new Account(name, age, 0, accounts.size() + 1);
+        accounts.add(newAccount);
+
+        LOGGER.debug("New account added: {} with ID: {}", newAccount.name, newAccount.accountId);
+    }
+
+    // Gets an account by ID
+    public static Account getAccountById(int accountId){
+        for(Account account : accounts){
+            if(account.accountId == accountId){
+                return account;
+            }
+        }
+        return null;
+    }
+
+
+
 }
